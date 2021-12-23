5b73cc13f692dd4841e955676b1178a377fd43d7
garret <garret@airmail.cc>
2021-12-23 17:26:40 +0000

    :

--- macros/garret.inverse-glow.lua
+++ macros/garret.inverse-glow.lua
@@ -1,7 +1,7 @@
 script_name = "Inverse Glow"
 script_description = "glow but it goes inside the letter"
 script_author = "garret"
-script_version = "1.0.0"
+script_version = "1.0.1"
 script_namespace = "garret.inverse-glow"
 
 local haveDepCtrl, DependencyControl, depctrl = pcall(require, "l0.DependencyControl")
@@ -22,25 +22,25 @@ local function main(subs, sel)
     for i = #sel, 1, -1 do
         local line = subs[sel[i]]
         local tags, text = line.text:match(patterns.tags_text)
-        local blur = string.match(tags, patterns.bord) -- \bord = value of \blur
+        local blur = tags:match(patterns.bord) -- \bord = value of \blur
         local dark = tags:match(patterns.dark) -- \c = colour of dark layer
         local light = tags:match(patterns.light) -- \3c = colour of light layer
 
         if tags and blur and dark and light then -- skip lines that don't have everything needed
-            tags = string.gsub(tags, patterns.bord, "") -- remove \bord
-            tags = string.gsub(tags, patterns.dark, "") -- and both colours
-            tags = string.gsub(tags, patterns.light, "")
+            tags = tags:gsub(patterns.bord, "") -- remove \bord
+            tags = tags:gsub(patterns.dark, "") -- and both colours
+            tags = tags:gsub(patterns.light, "")
 
             -- light layer
             local light_line = util.copy(line)
             light_line.layer = line.layer + 1
-            local light_tags = string.gsub(tags, "}", "\\bord0\\shad0\\c" .. light .. "}") -- } (end of tag block) --> \cLightColour}
+            local light_tags = tags:gsub("}", "\\bord0\\shad0\\c" .. light .. "}") -- } (end of tag block) --> \cLightColour}
             light_line.text = light_tags .. text
             subs[sel[i]] = light_line
 
             local dark_line = util.copy(line)
             dark_line.layer = line.layer + 2
-            local dark_tags = string.gsub(tags, "}", "\\bord0\\shad0\\c" .. dark .. "\\blur" .. blur .. "}") -- } -> \cDarkColour\blurBord}
+            local dark_tags = tags:gsub("}", "\\bord0\\shad0\\c" .. dark .. "\\blur" .. blur .. "}") -- } -> \cDarkColour\blurBord}
             dark_line.text = dark_tags .. text
             subs.insert(sel[i] + 1, dark_line)
         end
