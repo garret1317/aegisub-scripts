c4ea5def114b28123c0ee9795d4b141a0122a12c
garret <garret@airmail.cc>
2022-08-05 02:57:42 +0100

    append-comment: use textbox instead of edit
    newlines start new comments
    "type it yourself" message has been removed
    local variables have been remembered this time

--- macros/garret.append-comment.lua
+++ macros/garret.append-comment.lua
@@ -1,7 +1,7 @@
 script_name = "Append Comment"
 script_description = "{ts do all the work pls kthxbye}"
 script_author = "garret"
-script_version = "1.2.0"
+script_version = "2.0.0"
 script_namespace = "garret.append-comment"
 
 local haveDepCtrl, DependencyControl, depctrl = pcall(require, "l0.DependencyControl")
@@ -12,7 +12,7 @@ if haveDepCtrl then
 end
 
 local function main(sub, sel)
-    dialog_config=
+    local dialog_config=
     {
         {
             class="label",
@@ -20,19 +20,17 @@ local function main(sub, sel)
             label="Comment:"
         },
         {
-            class="edit",name="msg",
-            x=0,y=1,width=3,height=2,
+            class="textbox",name="msg",
+            x=0,y=1,width=10,height=5,
             value=""
         }
     }
-    if #sel == 1 then
-        table.insert(dialog_config, {class="label",x=0,y=3,width=1,height=1,label="if you're not using this for multiple lines \nyou may as well just type it out yourself"})
-    end
-    button, results = aegisub.dialog.display(dialog_config)
+    local button, results = aegisub.dialog.display(dialog_config)
     if button ~= false then
         for _, i in ipairs(sel) do
             local line = sub[i]
-            line.text = line.text.." {"..results.msg.."}"
+            local msg = results.msg:gsub("\n", "}{")
+            line.text = line.text.." {"..msg.."}"
             sub[i] = line
         end
     else
